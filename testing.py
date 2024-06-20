41bdff68aa423f92dc754d973423d759d4890659
MedLarg8 <larg8med@gmail.com>
2024-06-20 10:54:38 +0100

    adding empreinte digitale functions to block chain

--- testing.py
+++ testing.py
@@ -1,4 +1,4 @@
-from blockChain.tuto import Client, ClientInfo, Transaction
+from blockChain.tuto import Client, ClientInfo, Transaction, Block, mine, verify_signature, check_balance
 from flask import Flask
 from flask_mysqldb import MySQL
 import datetime
@@ -31,6 +31,8 @@ def create_Database_Transaction(transaction):
     cur = mysql.connection.cursor()
     cur.execute("INSERT INTO transactions (sender,recepient,value,date,signature) VALUES(%s,%s,%i,%d,%s)",
                 (sender,recepient,value,date,signature))
+    mysql.connection.commit()
+    cur.close()
     
 
 def create_database_client(client,client_info):
@@ -44,11 +46,83 @@ def create_database_client(client,client_info):
     cur.close()
 
 
+def execute_transaction(transaction):
+    assert isinstance(transaction, Transaction)
+
+    sender = transaction.sender
+    recepient = transaction.recipient
+    value = transaction.value
+
+    
+
+
+def pass_transaction(transactions):
+    global LAST_BLOCK_HASH
+    global LAST_TRANSACTION_INDEX
+    global ALL_TRANSACTIONS
+    print("LAST BLOCK HASH : ",LAST_BLOCK_HASH)
+    print("LAST TRANSACTION INDEX : ",LAST_TRANSACTION_INDEX)
+    if not transactions:
+        return
+    else :
+        ALL_TRANSACTIONS += transactions
+        print("size of all transaction: ",len(ALL_TRANSACTIONS))
+    
+    if not blockchain:
+        block = Block()
+    else:
+        if blockchain[-1].can_add_transaction(ALL_TRANSACTIONS[LAST_TRANSACTION_INDEX]):
+            block = blockchain = [-1]
+        else:
+            block = Block()
+
+    while(block.can_add_transaction(ALL_TRANSACTIONS[LAST_TRANSACTION_INDEX]) and LAST_TRANSACTION_INDEX<len(ALL_TRANSACTIONS)):
+        temp_transaction = ALL_TRANSACTIONS[LAST_TRANSACTION_INDEX]
+        print("transaction #",LAST_TRANSACTION_INDEX)
+        b1 = True
+        try:
+            verify_signature(temp_transaction)
+            print("signature verified")
+        except(ValueError,TypeError):
+            b1 = False
+            print("wrong signature")
+        
+        if b1 and check_balance(temp_transaction):
+            block.verified_transaction.append(temp_transaction)
+            execute_transaction(temp_transaction)
+        else:
+            print("non validated")
+        
+        print("pre incrementation")
+        LAST_TRANSACTION_INDEX +=1
+        print("post incrementation")
+        if(LAST_TRANSACTION_INDEX<len(ALL_TRANSACTIONS)):
+            if block.can_add_transaction(ALL_TRANSACTIONS[LAST_TRANSACTION_INDEX])==False:
+
+                block.previous_block_hash = LAST_BLOCK_HASH
+                block.Nonce = mine(block, 2)
+                digest = hash(block)
+                blockchain.append(block)
+                LAST_BLOCK_HASH = digest
+
+                block = Block()
+                print("new block added")
+            else:
+                print("working on the same block")
+        else:
+            break
+
+    blockchain.append(block)
+
+
 
 if __name__ == "__main__":
     with app.app_context():
-        Med = Client(5000)
-        Med_info = ClientInfo("mohamed", b"123456", "image")
-        print("************")
-        create_database_client(Med, Med_info)
-        print("************")
+        #Med = Client(5000)
+        #Med_info = ClientInfo("mohamed", b"123456", "image")
+        #print("************")
+        #create_database_client(Med, Med_info)
+        #print("************")
+        pass
+    pass
+    
